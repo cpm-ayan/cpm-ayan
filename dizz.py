f2bb1685512f62c64750ca403a88a09de60e58f1
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-05-24 11:55:57 +0500

    Update dizz.py

--- dizz.py
+++ dizz.py
@@ -12,7 +12,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmtooldev import CPMTooldev
+from carparktool import CarParkTool
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -124,7 +124,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMTooldev(acc_access_key)
+        cpm = CarParkTool(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
